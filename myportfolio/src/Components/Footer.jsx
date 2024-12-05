89dce01f157f875a1d018f33376d014ccdec974d
Thando <thandofuneka1@.com>
2024-12-05 21:29:16 +0200

    Footer

---
+++ myportfolio/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer () {
+    return (
+        <div className="footer">
+            <a href="mailto:thandofuneka1@gmail.com">Email Me</a>
+            <a href="https://github.com/thandofuneka">GitHub</a>
+            <a href="https://www.linkedin.com/in/thando-funeka-9b30ba256/">LinkedIn</a>
+
+        </div>
+    )
+}
+
+export default Footer;
