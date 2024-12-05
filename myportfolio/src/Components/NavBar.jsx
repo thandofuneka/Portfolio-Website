83a53dcd27cbf3f52302ac2b2bbce89adac4eddd
Thando <thandofuneka1@.com>
2024-12-05 20:36:13 +0200

    Added NavBar and react router dom

---
+++ myportfolio/src/Components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+    return (
+        <nav className='navbar'>
+            <div className='nav-content'>
+               <h1 className='nav-logo'>Thando Funeka</h1>
+
+               <div className='nav-links'>
+                <Link to='/' className='nav-link'>Home</Link>
+                <Link to='/Projects' className='nav-link'>Projects</Link>
+               </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
