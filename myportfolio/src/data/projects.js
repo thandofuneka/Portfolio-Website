f73db5d66f313b9887be983b95654290baa9be8d
Thando <thandofuneka1@.com>
2024-12-07 20:05:20 +0200

    Added VanLife project

--- myportfolio/src/data/projects.js
+++ myportfolio/src/data/projects.js
@@ -7,7 +7,7 @@ const projects = [
       description: 'It is a landing page website for a bank',
       repoLink: 'https://github.com/thandofuneka/SDF_Portfolio_Piece_CSC20240121_WFO2407_Group-D_Thando-Funeka_SDF11.git',
       image: '/favicon/images/CacheBank.png', 
-      technologies: ['React', 'Tailwind CSS', 'JavaScript'],
+      technologies: ['Tailwind CSS', 'JavaScript'],
     },
     {
       id: 2,
@@ -24,6 +24,14 @@ const projects = [
       repoLink: 'https://github.com/thandofuneka/CS20240121_WFO2407_Group-D_Thando-Funeka_DJS03.git',
       image: '/favicon/images/BookConnect.png' ,
       technologies: ['JavaScript', 'HTML', 'CSS'],
+    },
+    {
+      id: 4,
+      title: 'Van Life',
+      description: 'It is website for hiring vans it also includes a hosts page that requires logging in before use which stores the password and username in a database for future log-ins ',
+      repoLink: 'https://github.com/thandofuneka/CS20240121_WFO2407_Group-D_Thando-Funeka_DJS08.git',
+      image: '/favicon/images/VanLife.png',
+      technologies: ['JavaScript', 'HTML', 'CSS', 'Typescript', 'React'],
     },
     
   ];
